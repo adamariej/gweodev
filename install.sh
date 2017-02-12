a5ef829b968d7d929784de3d8d1a8a23524e4994
Julien Adam <adamj@paratools.com>
2017-02-12 17:11:37 +0100

    Continue install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-BUILD_PREFIX=
-INSTALL_PREFIX="$PWD"
-APP_INSTALL_PREFIX=
+BUILD_PREFIX=$PWD/build
+INSTALL_PREFIX=$PWD/build/install
+INSTALL_APP_PREFIX=
+DEPLOY_APPS=no
 SOURCE_PREFIX=$(readlink -f `dirname $0`)
 error()
 {
@@ -19,7 +20,7 @@ print_config()
 	printf "# PROJECT SRC INTO $SOURCE_PREFIX\n"
 	printf "# BUILDING    INTO $BUILD_PREFIX\n"
 	printf "# INSTALLING  INTO $INSTALL_PREFIX\n"
-	printf "# APP INSTALL INTO $APP_INSTALL_PREFIX\n"
+	printf "# APP INSTALL INTO $INSTALL_APP_PREFIX\n"
 	printf "\n"
 }
 
@@ -28,22 +29,73 @@ help_menu()
 	printf "#######################################\n"
 	printf "# Usage: ./install.sh"
 	printf "#######################################\n"
-	printf "# --prefix=<path>"
-	printf "# --bprefix=<path>"
-	printf "# --help"
+	printf "# --prefix=<path>\n"
+	printf "# --temp-prefix=<path>\n"
+	printf "# --with-apps=<path>\n"
+	printf "# --app-prefix=<path>\n"
+	printf "# --help|-h|-help\n"
 	printf "#######################################\n"
 	printf "\n"
 
 }
 
+deploy_apps()
+{
+	mkdir -p $BUILD_PREFIX
+	cd $BUILD_PREFIX
+
+	for app in `cat $SOURCE_PREFIX/url.txt`
+	do
+		app_name=`echo $app | cut -f1 -d';'`
+		app_url=`echo $app | cut -f2 -d';'`
+
+		if test -n "`echo $app_name | \egrep "^#.*$"`"; then
+			continue;
+		fi
+
+		l="`cat $SOURCE_PREFIX/configure.txt | \egrep "^$app_name;.*$"`"
+		app_before="`echo $l | cut -f2 -d";"`"
+		app_cargs="`echo $l | cut -f3 -d";"`"
+		app_margs="`echo $l | cut -f4 -d";"`"
+
+		rm -rf $app_name
+		echo git clone --depth 1 $app_url $app_name || error "Unable to clone $app_name"
+		#cd $app_name && mkdir -p build && cd build
+
+		if test -n "$app_before"; then
+			echo $app_before
+		fi
+		echo ../configure $app_cargs --prefix=$INSTALL_APP_PREFIX || error "Unable to run \'$app_name\' configure !"
+		echo make -j4 $app_margs || error "Unable to install \'$app_name\' software !"
+		echo
+
+	done
+}
+
+deploy_link()
+{
+	#deploy the env
+	if test ! -a $1 ; then
+		ln -sf $2 $1
+	else
+		error "Can't override $1: Is not our symlink !\n"
+	fi
+}
+
 for arg in $@
 do
 	case $arg in
-		--bprefix=*)
-			BUILD_PREFIX=$(read_arg "$arg" "--bprefix")
+		--temp-prefix=*)
+			BUILD_PREFIX=$(read_arg "$arg" "--tmp-prefix")
 			;;
 		--prefix=*)
 			INSTALL_PREFIX=$(read_arg "$arg" "--prefix")
+			;;
+		--with-apps)
+			DEPLOY_APPS=yes;
+			;;
+		--app-prefix=*)
+			INSTALL_APP_PREFIX=$(read_arg "$arg" "--app-prefix")
 			;;
 		--help|-h|-help)
 			help_menu
@@ -55,52 +107,40 @@ do
 	esac
 done
 
-
 test -z "$BUILD_PREFIX" && BUILD_PREFIX=`mktemp -d`
-APP_INSTALL_PREFIX="$INSTALL_PREFIX/.usr"
-
+test -z "$INSTALL_APP_PREFIX" && INSTALL_APP_PREFIX="$INSTALL_PREFIX/products"
 print_config
 
 ##### CHECKS ######
-
 # assert if trying to build or install in src tree
-test "$BUILD_PREFIX" = "$SOURCE_PREFIX" -o "$INSTALL_PREFIX" = "$SOURCE_PREFIX" && error "Can't build or install in SRC tree !\n"
+test "$BUILD_PREFIX"   = "$SOURCE_PREFIX" && error "Can't build our distrib directly in source tree !\n"
+test "$INSTALL_PREFIX" = "$SOURCE_PREFIX" && error "Can't install directly in SRC tree !\n"
+test "$DEPLOY_APPS" = "yes" -a "$INSTALL_APP_PREFIX" = "$SOURCE_PREFIX" && error "Can't install apps directly in SRC tree\n"
 
-# Check if we can deploy env/ solution
-test -e $INSTALL_PREFIX/.superbash -a ! -L $INSTALL_PREFIX/.superbash && error "Can't override \$HOME/.superbash: Is not our symlink !\n"
-
-# Check if we can deploy vim/ solution
-test -e $INSTALL_PREFIX/.vimrc -a ! -L $INSTALL_PREFIX/.vimrc && error "Can't override \$HOME/.vimrc: Is not our symlink !\n"
-
-# Check if we can deploy GDB solution
-test -e $INSTALL_PREFIX/.gdbinit -a ! -L $INSTALL_PREFIX/.gdbinit && error "Can't override \$HOME/.gdbinit: Is not our symlink !\n"
-
+if test "$DEPLOY_APPS" = "yes"; then
+	deploy_apps
+fi
 exit 0
 
 
-
-
-
-
-test ! -d $HOME/.superbash ||  error "SuperBash already deployed in your environment. Please use update.sh instead \n"
-test -d ./bash_env || error "This script should be run in root directory ! Abort !\n"
-
-#deploy the env
-ln -sf $PWD/bash_env $HOME/.superbash
-echo ". \$HOME/.superbash/bash_global" >> $HOME/.bashrc
-
-which vim > /dev/null 2>&1
-if test $? -eq 0
-then
-	printf " * VIM deployment\n"
-	mkdir -p $HOME/.vim/bundle
-	ln -sf $PWD/vim/vimrc $HOME/.vimrc
-	tar xf $PWD/vim/vundle.tar.gz -C $HOME/.vim/bundle
-	vim -e +PluginInstall +qall 2>/dev/null
-else
-	printf " * Skipping VIM deployment: vim not found\n"
-fi
-#deploy vim
+deploy_link "$INSTALL_PREFIX/.superenv" "$SOURCE_PREFIX/env"
+deploy_link "$INSTALL_PREFIX/.vimrc" "$SOURCE_PREFIX/vim/vimrc"
+deploy_link "$INSTALL_PREFIX/.tmux.conf" "$SOURCE_PREFIX/tmux/tmux.conf"
+deploy_link "$INSTALL_PREFIX/.gitconfig" "$SOURCE_PREFIX/git/gitconfig"
+deploy_link "$INSTALL_PREFIX/.gitattributes_global" "$SOURCE_PREFIX/git/gitattributes_global"
+deploy_link "$INSTALL_PREFIX/.gitignore_global" "$SOURCE_PREFIX/git/gititgnore_global"
+deploy_link "$INSTALL_PREFIX/.gdbinit" "$SOURCE_PREFIX/gdb/gdbinit"
 
 printf "Installation completed !\n"
+if test -n "$INSTALL_APP_PREFIX"; then
+	printf "To add products in your environment: please export\n"
+	printf "SUPERBASH_INSTALL=$INSTALL_PREFIX\n"
+	printf "PATH=\${SUPERBASH_INSTALL}/bin:\$PATH\n"
+	printf "INCLUDE_PATH=\${SUPERBASH_INSTALL}/include:\$INCLUDE_PATH\n"
+	printf "C_INCLUDE_PATH=\${SUPERBASH_INSTALL}/bin:\$C_INCLUDE_PATH\n"
+	printf "LD_LIBRARY_PATH=\${SUPERBASH_INSTALL}/lib64:\${SUPERBASH_INSTALL}/lib:\$LD_LIBRARY_PATH\n"
+	printf "MANPATH=\${SUPERBASH_INSTALL}/share/man:\$MANPATH\n"
+	printf ". $INSTALL_PREFIX/.superenv/bash_global"
+	printf "Think about deploying VIM plugins"
+fi
 
