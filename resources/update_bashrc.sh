a1311fc7dff8bc9c89b38041291305c4fcb2df3a
Julien Adam <adamj@paratools.com>
2017-10-26 05:44:26 -0700

    Sourcingw

--- resources/update_bashrc.sh
+++ resources/update_bashrc.sh
@@ -5,7 +5,7 @@ to_print()
 	thepath=`dirname $0`
 	thepath=`readlink -e $thepath`/..
 	echo "export GD_SRC=$thepath;"
-	echo "\${GD_SRC}/gweodenv/bash_global"
+	echo ". \${GD_SRC}/gweodenv/bash_global"
 }
 
 to_print >> ~/.bashrc
