dbc6cb02d92c99ac68e97912449236cd10a61df7
Julien Adam-Marie <julien.adam-marie@ansys.com>
2025-04-16 22:08:59 +0200

    Update

--- gweodenv/zsh/custom/1_functions.zsh
+++ gweodenv/zsh/custom/1_functions.zsh
@@ -49,3 +49,11 @@ function mpcbuild()
 	fi
 }
 
+
+function jwtd() {
+    if [[ -x $(command -v jq) ]]; then
+         jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< "${1}"
+         echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
+    fi
+}
+
--- gweodenv/zsh/custom/2_aliases.zsh
+++ gweodenv/zsh/custom/2_aliases.zsh
@@ -5,3 +5,17 @@ alias du="du -cksh"
 alias tailf="tail -f"
 alias e="code-insiders"
 alias prettyjson="python3 -m json.tool | pygmentize -l json"
+
+
+alias gitm="git -C ~/root2-main"
+alias gitw="git -C ~/root2"
+alias gitw1="git -C ~/root2-w1"
+alias gitw2="git -C ~/root2-w2"
+
+alias mtm="cd ~/root2-main"
+alias mtw="cd ~/root2"
+alias mtw1="cd ~/root2-w1"
+alias mtw2="cd ~/root2-w2"
+
+alias k="kubectl"
+alias kv="k9s"
